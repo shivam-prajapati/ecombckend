88a8579ffb6c9a0043d954632253d40e1b3b2963
shivamprajapatigola@gmail.com <shivamprajapatigola@gmail.com>
2024-06-11 11:21:05 +0530

    added login/signup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const bcrypt = require("bcrypt");
 const asyncHandler = require("express-async-handler");
 dotenv.config();
 const bodyParser = require("body-parser");
@@ -13,24 +14,45 @@ const PORT = process.env.PORT || 5001;
 app.post(
   "/signup",
   asyncHandler(async (req, res) => {
-    const { name, mail, password } = req.body;
+    let { name, mail, password } = req.body;
+    const isUser = await User.find({ mail: mail });
+    if (isUser.length > 0) {
+      console.log("inside");
+      res.status(400).json({
+        message: "signup failed",
+        error: `user already exits with mail ${mail}`,
+      });
+      return;
+    }
     //mail should be new
     //save
-    const createdUser = await User.create({ name, mail, password });
+    const hashed = await bcrypt.hash(password, 10);
+    console.log("pass ", hashed);
+    const createdUser = await User.create({ name, mail, password: hashed });
     console.log("user created is", createdUser);
-    res.status(200).json({
-      name,
-      message: "got signup request",
-      mail,
-      password,
-    });
+    res.status(200).json(createdUser);
   })
 );
 
-app.post("/login", (req, res) => {
-  const { name, password } = req.body;
-  res.status(201).send({ message: "got login request", name, password });
-});
+app.post(
+  "/login",
+  asyncHandler(async (req, res) => {
+    const { mail, password } = req.body;
+    const isUser = (await User.find({ mail }))[0];
+    const isOk = await bcrypt.compare(password, isUser.password);
+    if (!isOk) {
+      res.status(400).json({
+        message: "can not login",
+        error: "password or mail is wrong",
+      });
+      return;
+    }
+    console.log("inside login");
+    isUser.message = "success login request";
+    console.log("logged in user is " ,isUser)
+    res.status(201).send(isUser);
+  })
+);
 app.get("/", (req, res) => {
   console.log("inside the first get");
   res.send({ hi: "this is JSON" });
